9ce8f942fedd69217d87c09ec58492c5f35a0eae
Andrefer-Santtos <1andre.santos.souza1@gmail.com>
2022-01-25 15:28:38 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/oic/cursomc/services/DBService.java
+++ src/main/java/com/oic/cursomc/services/DBService.java
@@ -110,7 +110,7 @@ public class DBService {
 		estadorepository.saveAll(Arrays.asList(est1, est2));
 		cidaderepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "", "email", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "André Santos", "email", "36378912377", TipoCliente.PESSOAFISICA);
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		
