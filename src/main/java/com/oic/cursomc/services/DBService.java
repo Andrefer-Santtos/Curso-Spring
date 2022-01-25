0408e2bd3edd698019f855bc4d89d09d8a4b6588
Andrefer-Santtos <1andre.santos.souza1@gmail.com>
2022-01-25 16:03:18 -0300

    Adicionando senha a Cliente

--- src/main/java/com/oic/cursomc/services/DBService.java
+++ src/main/java/com/oic/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.oic.cursomc.domain.Categoria;
@@ -33,6 +34,8 @@ import com.oic.cursomc.repositories.ProdutoRepository;
 @Service
 public class DBService {
 
+	@Autowired
+	private BCryptPasswordEncoder pe;
 	@Autowired
 	private CategoriaRepository categoriarepository;
 	@Autowired
@@ -110,7 +113,7 @@ public class DBService {
 		estadorepository.saveAll(Arrays.asList(est1, est2));
 		cidaderepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "André Santos", "email", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "André Santos", "email", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		
